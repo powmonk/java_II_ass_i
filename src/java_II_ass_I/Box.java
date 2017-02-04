706e777a200be2f3f2f0474eb70a263088130928
powmonk <dan@aldron.co.uk>
2017-02-04 11:06:16 +0000

    save

--- src/java_II_ass_I/Box.java
+++ src/java_II_ass_I/Box.java
@@ -1,6 +1,10 @@
 package java_II_ass_I;
 
-public class Box {
+public class Box extends Rectangle{
+	public Box(double lengthValue, double widthValue) {
+		super(lengthValue, widthValue);
+	}
+	
 	public Box(double lengthValue, double widthValue, double heightValue){
 		length = lengthValue;
 		width  = widthValue;
@@ -12,11 +16,12 @@ public class Box {
 	}
 	
 	public double surfaceArea(){
+		return 2*(length*width + length*height + width*height);
 		
 	}
 	
 	public double bodyDiagonalLength(){
-		
+		return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2) + Math.pow(height, 2));
 	}
 	
 	public double angle(){
